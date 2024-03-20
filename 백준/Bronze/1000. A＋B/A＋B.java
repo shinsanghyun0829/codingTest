7cbe67392657c5793560fadb6c2c8ff94d77c701
신상현 <137140907+shinsanghyun0829@users.noreply.github.com>
2024-03-20 22:12:35 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner Scanner = new Scanner(System.in);
+        int a = Scanner.nextInt();
+        int b = Scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
